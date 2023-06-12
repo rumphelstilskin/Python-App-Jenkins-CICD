68dbcb9d490f1228f90ea56f4ffcddcfb459141d
rumphelstilskin <99741914+rumphelstilskin@users.noreply.github.com>
2023-06-12 17:26:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello World!</p>"
+@app.route("/test-page")
+def test_page():
+    return "<p>This page is a test page</p>"
 
 app.run(debug=True, use_reloader=True, host="0.0.0.0")
